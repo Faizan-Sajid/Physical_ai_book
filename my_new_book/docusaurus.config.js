c50ded08c069f85a19c24dbfa382caf3f6612c04
faizan sajid <165155018+Faizan-Sajid@users.noreply.github.com>
2025-12-07 21:01:30 +0500

    edit

--- my_new_book/docusaurus.config.js
+++ my_new_book/docusaurus.config.js
@@ -97,7 +97,7 @@ const config = {
     },
     {to: '/blog', label: 'Blog', position: 'left'},
     {
-      href: 'https://github.com/facebook/docusaurus',
+      href: 'https://github.com/Faizan-Sajid/Physical_ai_book',
       label: 'GitHub',
       position: 'right',
     },
@@ -142,7 +142,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/Faizan-Sajid/Physical_ai_book',
               },
             ],
           },
